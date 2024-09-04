e72cba6c1dc29d2361b01869358270a2bde44881
David Gonçalves <david.a.p.g@hotmail.com>
2024-09-04 14:58:01 -0300

    alteracao

--- src/pages/estagio/Estagio.jsx
+++ src/pages/estagio/Estagio.jsx
@@ -4,14 +4,14 @@ import style from './Estagio.module.css'
 export const Estagio = () => {
     return (
         <div className={style.Estagio}>
-            <div className={style.Menu}>
+            <div className={style.Menu2}>
                 <MenuRetravel />
             </div>
             <div className={style.MenuEstagio}>
                 <MenuRetravelModalidade />
             </div>
             <div className={style.Caixa}>
-                <h1 className={style.Title}>Área Aluno: Modalidade Estágio</h1>
+                <h1 className={style.Title2}>Área Aluno: Modalidade Estágio</h1>
                 <TabelaAluno/>
             </div>
         </div>
