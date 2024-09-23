25edcc79279621c23bc84d4a2da11bf3c2339e9b
David Gonçalves <david.a.p.g@hotmail.com>
2024-09-23 16:46:05 -0300

    ajustando erro 404

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/projeto-modalidade-fatec-campinas-aluno/',
+  base: '/projeto-modalidade-fatec-campinas-aluno/',  // Certifique-se de usar o nome correto do repositório.
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
