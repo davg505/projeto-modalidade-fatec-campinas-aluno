885e4057fb004f8f901fb734868f80834de4faab
David Gonçalves <david.a.p.g@hotmail.com>
2024-09-23 16:54:57 -0300

    atualizar 404

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,17 +1,17 @@
+
+
 import { Route, Routes } from "react-router-dom";
 import { LayoutPadrao } from "./layout/layoutPadrao/LayoutPadrao";
 import { Error404, Estagio, Inicial } from './pages';
 
-
 export const Router = () => {
     return (
         <Routes>
-            <Route path="/" element={<LayoutPadrao/>}>
-            <Route path="/" element={<Inicial/>}/>
-            <Route path="/estagio" element={<Estagio/>}/>
+            <Route path="/" element={<LayoutPadrao />}>
+                <Route index element={<Inicial />} /> {/* Rota padrão para '/' */}
+                <Route path="estagio" element={<Estagio />} />
             </Route>
-            <Route path="*" element={<Error404/>}/>
+            <Route path="*" element={<Error404 />} /> {/* Captura qualquer rota não encontrada */}
         </Routes>
-
     );
 };
